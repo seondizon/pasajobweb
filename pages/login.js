fea3e965532fda35e3150a6a5cbe9cdb088e659f
seon <josemadizon@gmail.com>
2020-02-05 02:34:13 +0800

    login request

--- pages/login.js
+++ pages/login.js
@@ -3,12 +3,91 @@ import { Form, Icon, Input, Button, Col, Row, Checkbox } from 'antd';
 import Layout from '../Layouts/register'
 import FormItem from 'antd/lib/form/FormItem';
 import Link from 'next/link';
+import axios from 'axios';
+import gql from 'graphql-tag'
+import { print } from 'graphql'
+// import { request } from 'request'
+
+const LOGIN = gql`query MyQuery {
+	auth_login(object: {email: "johnmaclained@gmail.com", password: "123456"}) {
+    token
+ }
+}`;
 
 const Login = (props) => {
 
+  const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = props.form;
+  
+  // const uri = 'https://auth.pasajob.apolidata.com/graphql';
+  // const apolloFetch = createApolloFetch({ uri });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+      props.form.validateFields( async (error, values) => {
+        if (!error) {
+          // console.log( "asdasd", values )
+
+
+
+          var myHeaders = new Headers();
+          myHeaders.append("Content-Type", "application/json");
+
+          var graphql = JSON.stringify({
+            query: "query MyQuery(  $username:String!, $password:String! ) {\n	auth_login(object: {email: $username, password: $password}) {\n    token\n }\n}",
+            variables: {"username":"johnmaclained@gmail.com","password":"123456"}
+          })
+          var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: graphql,
+            redirect: 'follow'
+          };
+
+          await fetch("https://auth.pasajob.apolidata.com/graphql", requestOptions)
+            .then(response => console.log( response.text() ))
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+
+
+          // apolloFetch.use(({ request, options }, next) => {
+          //   if (!options.headers) {
+          //     options.headers = { 
+          //       "Access-Control-Allow-Origin" : "*",
+          //       "Content-Type" : "application/json"
+          //     };  // Create the headers object if needed.
+          //   }
+          //   options.headers['authorization'] = 'created token';
+          
+          //   next();
+          // });
+
+          // apolloFetch({ LOGIN })
+          //   .then(res => console.log("ret", res))
+          //   .catch(err => console.log("fa", err))
+
+          // await axios.post("https://auth.pasajob.apolidata.com/graphql", {
+          //   headers : { "Access-Control-Allow-Origin" : "*" }
+          // }, {
+          //   query:print( LOGIN ),
+          //   variables: {
+          //     username: values.username,
+          //     password: values.password
+          //   }
+          // })
+          // .then(res => console.log("ret", res))
+          // .catch(err => console.log("fa", err))
+
+
+          
+
+          
+        }
+      });
+  }
+
   const LoginForm = () => (
 
-    <Form>
+    <Form onSubmit={ handleSubmit } >
 
           <Row>
             <Col span={ 24 }>
@@ -16,7 +95,18 @@ const Login = (props) => {
                 name=""
                 label="Email / Username"
                 >
-                  <Input placeholder="Username"/>
+
+
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+            })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />,
+          )}
+
+
               </FormItem>
             </Col>
           </Row>
@@ -27,7 +117,17 @@ const Login = (props) => {
                 name=""
                 label="Password"
                 >
-                  <Input type="password" placeholder="Password"/>
+                  
+                  { getFieldDecorator('password', {
+                    rules: [{ required: true, message: 'Please input your Password!' }],
+                  })(
+                    <Input
+                      prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      type="password"
+                      placeholder="Password"
+                    />,
+                  ) }
+
               </FormItem>
             </Col>
           </Row>
@@ -43,7 +143,7 @@ const Login = (props) => {
 
           <Row>
             <Col>
-              <Button type="primary" block>
+              <Button type="primary" block htmlType="submit" >
                 Login
               </Button>
             </Col>
@@ -55,6 +155,7 @@ const Login = (props) => {
 
 
   return (
+    
     <Layout title="Login" pageId="loginPage" >
     <div className="login-form">
       { LoginForm() }
@@ -64,4 +165,4 @@ const Login = (props) => {
   
 }
 
-export default Login
+export default Form.create()(Login)
